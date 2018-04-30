29890343858359aeb417b9b84e046325e7b81879
M.Cemil Guney <mcemilguneyy@gmail.com>
2018-04-30 21:50:47 +0300

    Add small script for installation plugins.

---
+++ vim_up.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cp .vimrc ~/
+
+# NerdTree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# ctrlp
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+
+# airline
+git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline
+
+# vim-colors-solarized
+git clone https://github.com/altercation/vim-colors-solarized ~/.vim/bundle/vim-colors-solarized
+
+
